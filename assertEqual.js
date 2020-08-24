20c868016a4b6e5ce1536908ffb9ccaa7da928d9
Michael Klepac <mklepac90@gmail.com>
2020-08-24 16:52:59 +0000

    refactoring test cases

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('Michael', 'Michael');
-assertEqual('Michael', 'michael');
-assertEqual(3, 4);
+module.exports = assertEqual;
