2d3f28ba51aea02be2c1129f2982e4522dc4ed59
Michael Klepac <mklepac90@gmail.com>
2020-08-24 16:53:36 +0000

    contains test modules for lotide project. Initial commit.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('Michael', 'Michael');
+assertEqual('Michael', 'michael');
+assertEqual(3, 4);
