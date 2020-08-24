265ebafecf534fbf77fe2d38a3fc661f22d5fb07
Michael Klepac <mklepac90@gmail.com>
2020-08-24 17:16:36 +0000

    move test code to new module

--- tail.js
+++ tail.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1,);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
